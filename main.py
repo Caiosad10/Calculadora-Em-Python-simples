0cbba71087184b5d4ad87b46a2d073d0f724fd8c
Caiosad10 <150187420+Caiosad10@users.noreply.github.com>
2024-05-18 16:08:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def calculadora(num1, num2):
+  operacao = input('Qual o operação voce gostaria de fazer (+; -; *; /)? ')
+  if operacao == '+':
+    return f'O resultado da soma é {num1 + num2}'
+  elif operacao == '-':
+    return f'O resultado da subtração é {num1 - num2}'
+  elif operacao == '*':
+    return f'O resultado da multiplicação é {num1 * num2}'
+  elif operacao == '/':
+    return f'O resultado da divisão é {num1 / num2}'
+
+num1 = int(input('Digite o primeiro numero: '))
+num2 = int(input('Digite o segundo numero: '))
+
+print(calculadora(num1, num2))
